6802aea687f2968204aa63d46f2585d596cb40e4
Junpeng Chen <jche432@student.monash.edu>
2018-03-24 04:59:27 +1100

    now matching all routes

--- client/webserver.js
+++ client/webserver.js
@@ -3,9 +3,10 @@ const app = express()
 const path = require('path');
 
 app.use(express.static(__dirname + '/build'));
-
+app.use('/*',express.static(__dirname + '/build'));
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build/index.html'))
+    res.sendFile(path.resolve(__dirname+'/build/index.html'))
 });
+
 
 app.listen(80,()=>console.log('Server listenning on port 80..'))
