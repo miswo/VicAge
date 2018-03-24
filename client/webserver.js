d11e4581b59bb9c62328c859c5a16ca8fa53ebd9
Ubuntu <wysdom@vicage-host.d50zylgoctwudddbpwvkdqk2te.qx.internal.cloudapp.net>
2018-03-24 06:33:06 +0000

    v1.2 - changed webserver port to 80

--- client/webserver.js
+++ client/webserver.js
@@ -3,7 +3,6 @@ const app = express()
 const path = require('path');
 
 app.use(express.static(__dirname + '/build'));
-app.use('/*',express.static(__dirname + '/build'));
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname+'/build/index.html'))
 });
