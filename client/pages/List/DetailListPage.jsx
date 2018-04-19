d30cbf69d9040c6a97346c8026550baaf830db2a
Junpeng Chen <jche432@student.monash.edu>
2018-04-20 07:18:11 +1000

    v1.4 release added survey

--- client/pages/List/DetailListPage.jsx
+++ client/pages/List/DetailListPage.jsx
@@ -18,7 +18,7 @@ export default class DetailListPage extends React.Component{
                 this.setState({
                     list:res.data.list
                 })
-            })  
+            })
     }
 
     renderConcepts(){
