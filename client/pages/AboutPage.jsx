70abf320694fb7f06d91e7393aaf360d850988fb
Miswo <ichigoichie09@gmail.com>
2018-04-22 22:42:52 +1000

    className changed

--- client/pages/AboutPage.jsx
+++ client/pages/AboutPage.jsx
@@ -34,47 +34,47 @@ const AboutPage=()=>(
 
             <div className="project-description">
 
-                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                  <div class="panel panel-default">
-                    <div class="panel-heading" role="tab" id="headingOne">
-                      <h4 class="panel-title">
+                <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                  <div className="panel panel-default">
+                    <div className="panel-heading" role="tab" id="headingOne">
+                      <h4 className="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         Our Mission
                         </a>
                       </h4>
                     </div>
-                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                      <div class="panel-body">
+                    <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                      <div className="panel-body">
                         <p>The state of Victoria is one of the states in Australia with the most number of aged people and number of aged care facilities.</p>
                         <p>The website 'VicAge' aims at understanding the situation of "Healthy is..." with respect to an aged care scenario.</p>
                         <p>This process will help the elderly stay active and connected to the community around them which will provide them with an added sense of happiness.</p>
                       </div>
                     </div>
                   </div>
-                  <div class="panel panel-default">
-                    <div class="panel-heading" role="tab" id="headingTwo">
-                      <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  <div className="panel panel-default">
+                    <div className="panel-heading" role="tab" id="headingTwo">
+                      <h4 className="panel-title">
+                        <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                         Overview of the Scene of Aged care in Victoria
                         </a>
                       </h4>
                     </div>
                     <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-                      <div class="panel-body">
+                      <div className="panel-body">
                         <p>Something here</p>
                       </div>
                     </div>
                   </div>
-                  <div class="panel panel-default">
-                    <div class="panel-heading" role="tab" id="headingThree">
-                      <h4 class="panel-title">
-                        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  <div className="panel panel-default">
+                    <div className="panel-heading" role="tab" id="headingThree">
+                      <h4 className="panel-title">
+                        <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                         Our Solution - Communication tool using pictures
                         </a>
                       </h4>
                     </div>
-                    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-                      <div class="panel-body">
+                    <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                      <div className="panel-body">
                       <p>Are you struggling to decide activities for your care home regidents? We have a solution.
                         Our survey tool will minimise your job. It provides visual aids, which are easy to understand for everybody without explaining with a thousand words.
                         Sometime it is difficult to communicate with people who have difficulties of verval communication.
