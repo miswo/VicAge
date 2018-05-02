a6718b7c24d49545af6d6668e3e6a1b41775d051
Junpeng Chen <jche432@student.monash.edu>
2018-05-03 03:39:37 +1000

    added nutrition requirment in profile page also seperated calculator

--- client/pages/User/ProfilePage.jsx
+++ client/pages/User/ProfilePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 
+var util = require('../../util/util');
 
 export default class ProfilePage extends React.Component{
 
     constructor(props){
         super(props);
         this.state={
-
+            profile:{},
         }
     }
 
@@ -15,7 +16,7 @@ export default class ProfilePage extends React.Component{
         axios.get(this.props.serverURL+'/user/profile/'+this.props.user.id)
         .then((res)=>{
             this.setState({
-                profile:res.data.profile
+                profile:res.data.profile,
             })
         })
     }
@@ -74,6 +75,27 @@ export default class ProfilePage extends React.Component{
         })
     }
 
+    renderNutritionRequirement(){
+        var profile = this.state.profile;
+        var nutritionRequirements = util.NutritionRequirementCalculator(profile.age,
+                                                                        profile.gender,
+                                                                        profile.height,
+                                                                        profile.weight,
+                                                                        profile.activeLevel);
+        
+        const requirements =
+        <div>
+            <p>Calcium: {nutritionRequirements.calcium} mg</p>
+            <p>Fiber: {nutritionRequirements.fiber} g</p>
+            <p>Fat: {nutritionRequirements.fat} g</p>
+            <p>Protein: {nutritionRequirements.protein} g</p>
+            <p>Calorie: {nutritionRequirements.calorie} kcal</p>
+
+        </div>
+
+        return requirements;
+    }
+
     render(){
         return(
             <div id="profile-page">
@@ -86,103 +108,119 @@ export default class ProfilePage extends React.Component{
 
 
                 <div className="container">
-                    <div className="query-box">
-                        <h3>Set Profile of Your Dependant</h3>
-                        <form className="form" onSubmit={this.onSubmitInputProfile.bind(this)}>
-                            <div className="row">
-                                <div className="form-group">
-                                    <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
-                                        <label htmlFor="input-age">Age:</label>
-                                    </div>
-                                    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                                        <input required  value={this.state.profile?this.state.profile.age:undefined} 
-                                                onChange = {this.onValueChange.bind(this)} 
-                                                className="form-control query-input" 
-                                                type="number" 
-                                                id="input-age" />
-                                    </div>
-                                </div>
-                            </div>
+                    <div className="row">
 
-                            <div className="row">
-                                <div className="form-group">
-                                    <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div className="query-box">
+                                <h3>Set Profile of Your Dependant</h3>
+                                <form className="form" onSubmit={this.onSubmitInputProfile.bind(this)}>
+                                    <div className="row">
+                                        <div className="form-group">
+                                            <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+                                                <label htmlFor="input-age">Age:</label>
+                                            </div>
+                                            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                                                <input required  value={this.state.profile?this.state.profile.age:undefined} 
+                                                        onChange = {this.onValueChange.bind(this)} 
+                                                        className="form-control query-input" 
+                                                        type="number" 
+                                                        id="input-age" />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                        <div className="form-group">
+                                            <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+                                            
+                                                <label htmlFor="input-gender">Gender:</label>
+                                            </div>
+                                            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                                                <select required  onChange = {this.onValueChange.bind(this)} 
+                                                        value={this.state.profile?this.state.profile.gender+'':undefined}
+                                                        className="form-control query-input" 
+                                                        name="input-gender" 
+                                                        id="input-gender">
+                                                    <option value="male" >Male</option>
+                                                    <option value="female">Female</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                    <div className="form-group">
+                                        <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
                                     
-                                        <label htmlFor="input-gender">Gender:</label>
+                                            <label htmlFor="input-height">Height:</label>
+                                        </div>
+                                        <div className="col-xs-9 col-sm-9 col-md-8 col-lg-8">
+                                        
+                                            <input required onChange = {this.onValueChange.bind(this)} 
+                                                    value={this.state.profile?this.state.profile.height:undefined}  
+                                                    className="form-control query-input" 
+                                                    type="number" 
+                                                    id="input-height" />
+                                            <label>cm</label>
+                                        </div>
                                     </div>
-                                    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                                        <select required  onChange = {this.onValueChange.bind(this)} 
-                                                value={this.state.profile?this.state.profile.gender+'':undefined}
-                                                className="form-control query-input" 
-                                                name="input-gender" 
-                                                id="input-gender">
-                                            <option value="male" >Male</option>
-                                            <option value="female">Female</option>
-                                        </select>
                                     </div>
-                                </div>
+
+
+                                    <div className="row">
+                                        <div className="form-group">
+                                            <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+                                                <label htmlFor="input-weight">Weight:</label>
+                                            </div>
+                                            <div className="col-xs-9 col-sm-9 col-md-8 col-lg-8">
+                                                <input required onChange = {this.onValueChange.bind(this)} 
+                                                        value={this.state.profile?this.state.profile.weight:undefined} 
+                                                        className="form-control query-input" 
+                                                        type="number" 
+                                                        id="input-weight" />
+
+                                                <label>kg</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="row">
+                                        <div className="form-group">
+                                            <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+                                        
+                                                <label htmlFor="input-active-level">Active Level:</label>
+                                            </div>
+                                            <div className="col-xs-9 col-sm-9 col-md-6 col-lg-6">
+                                                <select required onChange = {this.onValueChange.bind(this)} 
+                                                        value={this.state.profile?this.state.profile.activeLevel+'':undefined}
+                                                        className="form-control query-input" 
+                                                        name="input-active-level" 
+                                                        id="input-active-level">
+                                                    <option value="1.2">No Exercise</option>
+                                                    <option value="1.375">Exercise 1-3 Days a Week</option>
+                                                    <option value="1.55">Exercise 3-5 Days a Week</option>
+                                                    <option value="1.725">Exercise 6-7 Days a Week</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" className="btn btn-primary">Save Profile</button>
+                                </form>                        
+                            </div>
+                        </div>
+
+                        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div className="query-box">
+                                <h3>Nutrition Requirement</h3>
+                                {this.renderNutritionRequirement()}
                             </div>
 
-                            <div className="row">
-                            <div className="form-group">
-                                <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
-                            
-                                    <label htmlFor="input-height">Height:</label>
-                                </div>
-                                <div className="col-xs-9 col-sm-9 col-md-8 col-lg-8">
-                                
-                                    <input required onChange = {this.onValueChange.bind(this)} 
-                                            value={this.state.profile?this.state.profile.height:undefined}  
-                                            className="form-control query-input" 
-                                            type="number" 
-                                            id="input-height" />
-                                    <label>cm</label>
-                                </div>
-                            </div>
-                            </div>
+                        </div>
 
-
-                            <div className="row">
-                                <div className="form-group">
-                                    <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
-                                        <label htmlFor="input-weight">Weight:</label>
-                                    </div>
-                                    <div className="col-xs-9 col-sm-9 col-md-8 col-lg-8">
-                                        <input required onChange = {this.onValueChange.bind(this)} 
-                                                value={this.state.profile?this.state.profile.weight:undefined} 
-                                                className="form-control query-input" 
-                                                type="number" 
-                                                id="input-weight" />
-
-                                        <label>kg</label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="row">
-                                <div className="form-group">
-                                    <div className="col-xs-3 col-sm-3 col-md-2 col-lg-2">
-                                
-                                        <label htmlFor="input-active-level">Active Level:</label>
-                                    </div>
-                                    <div className="col-xs-9 col-sm-9 col-md-6 col-lg-6">
-                                        <select required onChange = {this.onValueChange.bind(this)} 
-                                                value={this.state.profile?this.state.profile.activeLevel+'':undefined}
-                                                className="form-control query-input" 
-                                                name="input-active-level" 
-                                                id="input-active-level">
-                                            <option value="1.2">No Exercise</option>
-                                            <option value="1.375">Exercise 1-3 Days a Week</option>
-                                            <option value="1.55">Exercise 3-5 Days a Week</option>
-                                            <option value="1.725">Exercise 6-7 Days a Week</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <button type="submit" className="btn btn-primary">Save Profile</button>
-                        </form>                        
+                                            
                     </div>
+
                 </div>
             </div>
         )
---
+++ client/util/util.js
@@ -0,0 +1,53 @@
+exports.NutritionRequirementCalculator = (age,gender,height,weight,activeLevel)=>{
+        var calcium = 0;
+        if(age>=71)
+            calcium = 1200;
+        else if(age >=51 && gender ==='female')
+            calcium = 1200;
+        else if(age >=51 && gender ==='male')
+            calcium = 1000;
+        else if(age >= 19)
+            calcium = 1000;
+        else if(age >= 9)
+            calcium = 1300;
+        else if(age >= 4)
+            calcium = 1000;
+        else if(age >= 1)
+            calcium = 700;
+        else 
+            calcium = 260
+
+
+        var fiber = 0;
+        if(gender === 'male')
+            if(age>=51)
+                fiber=30;
+            else
+                fiber=38;
+        if(gender === 'female')
+            if(age>=51)
+                fiber= 21;
+            else    
+                fiber= 25;
+
+        var fat = '';
+        if(age >20)
+            fat = 20;
+        else if (age>4)
+            fat = 30;
+        else 
+            fat = 40;
+
+        var protein =0;
+        protein = Math.round(weight * 0.8,2);
+
+        var bmr = 0;
+        if(gender ==='male')
+            bmr = 10 * weight + 6.25 * height - 5 * age +5;
+        if(gender ==='female')
+            bmr = 10 * weight  + 6.25 * height - 5* age -161;
+
+        var calorie = Math.round(bmr * activeLevel,2);
+
+        return {calcium,fiber,fat,protein,calorie};
+}
