a4ccea84085e12b87bcc3b9670a77081dd91aedd
Miswo <ichigoichie09@gmail.com>
2018-05-07 01:13:08 +1000

    LandingPage mobile fix

--- client/pages/User/UserCenterPage.jsx
+++ client/pages/User/UserCenterPage.jsx
@@ -12,6 +12,13 @@ const FunctionSquare = (title,url,spanName)=>(
 
 );
 
+
+
+
+
+
+
+
 export default class UserCenterPage extends React.Component{
     constructor(props){
         super(props);
@@ -24,23 +31,69 @@ export default class UserCenterPage extends React.Component{
 
     render(){
         return(
+
+
+
+
             <div id="user-center-page">
                 <div className="jumbotron banner">
                     <div className="container">
-                        <h2>{this.props.user?this.props.user.profile.name + '\'s Profile':'Profile Center'}</h2>
+                        <h2>User Center</h2>
                     </div>
                 </div>
 
                 <div className="container">
                     <div className="row">
-                        {FunctionSquare('Edit Profile','/user/profile')}
-                        {FunctionSquare('Nutrition Planner','/planner')}
-                        {FunctionSquare('Activity Calendar','/goal/calendar')}
-                        {FunctionSquare('Plan Report','/planner/report')}
+                        <div className="col-md-3">
+                        <NavLink to={"/planner"}>
+                            <div className="concept-block" id="listImgColor">
+                                <div className="img-wrapper">
+                                    <img className="userCenterImg" src="http://d-ash.lolipop.jp/img/pizza.png" />
+                                </div>
+                                <h3>Dietary Planner</h3>
+                            </div>
+                        </NavLink>  
                     </div>
-                   
+
+                  
+                        <div className="col-md-3">
+                            <NavLink to={"/planner"}>
+                            <div className="concept-block" id="listImgColor">
+                                <div className="img-wrapper">
+                                    <img className="userCenterImg" src="http://d-ash.lolipop.jp/img/exercise.png" />
+                                </div>
+                                <h3>Exercise Planner</h3>
+                            </div>
+                            </NavLink>
+                        </div>
+
+                        <div className="col-md-3">
+                            <NavLink to={"/planner"}>
+                            <div className="concept-block" id="listImgColor">
+                                <div className="img-wrapper">
+                                    <img className="userCenterImg" src="http://d-ash.lolipop.jp/img/happy.png" />
+                                </div>
+                                <h3>Activity Scheduler</h3>
+                            </div>
+                            </NavLink>
+                        </div>
+
+                        <div className="col-md-3">
+                            <NavLink to={"/planner"}>
+                            <div className="concept-block" id="listImgColor">
+                                <div className="img-wrapper">
+                                    <img className="userCenterImg" src="http://d-ash.lolipop.jp/img/monitor.png" />
+                                </div>
+                                <h3>Health Status Monitor</h3>
+                            </div>
+                            </NavLink>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
+          
         )
     }
 
