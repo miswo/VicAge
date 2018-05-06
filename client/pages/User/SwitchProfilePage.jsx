780239230b014ad96692450261fa1f96b5d430db
Junpeng Chen <jche432@student.monash.edu>
2018-05-06 15:20:52 +1000

    v2.3 added register, new dynamic navbar and landing page

---
+++ client/pages/User/SwitchProfilePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+export default class SwitchProflePage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+
+
+    render(){
+        return(
+            <div id="switch-user-page">
+                <div className="">
+                
+                </div>
+            </div>
+        )
+    }
+}
